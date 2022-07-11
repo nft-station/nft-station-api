21134f153aa48e13c9c7f442e15eca75c4fc7147
Thanh Hao <lthao1986@gmail.com>
2022-07-11 09:13:20 +0700

    Set parameter optional

--- src/share/constants/api.const.ts
+++ src/share/constants/api.const.ts
@@ -2,7 +2,7 @@ export const CONTROLLER_CONSTANTS = {
   APP: { NAME: 'app', API_TAG: 'Application' },
   DASHBOARD: {
     NAME: 'dashboard', API_TAG: 'Dashboard', METHODS: {
-      BALANCES: 'balances/:address/:pagekey'
+      BALANCES: 'balances/:address/:pagekey?'
     }
   }
 };
