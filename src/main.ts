ac45ee62697385479b78179c8b0739062e6df16a
lthao1986 <lthao1986@gmail.com>
2022-07-16 08:37:12 +0700

    Test Heroku enviroment

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import { Inject } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import registerSwagger from './share/configurations/swagger.config';
-import { ConfigService } from '@nestjs/config';
 import { APP_CONFIG } from './share/configurations/app.config';
+import registerSwagger from './share/configurations/swagger.config';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
+
+  console.log('App config');
+  console.log(APP_CONFIG);
 
   app.setGlobalPrefix(APP_CONFIG.APP.API_REFIX);
   app.enableCors();
